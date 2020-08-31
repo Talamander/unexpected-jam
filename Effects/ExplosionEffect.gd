42dc3fe35130816d6b4e68ba7b6ff5ba1fdff446
Talamander <toxcrewproductions@gmail.com>
2020-08-31 15:40:41 -0500

    Particles now fade out.
    I had to set the particle system to one-shot and make sure to set their emitting to true in the gdscript of the effect

--- Effects/ExplosionEffect.gd
+++ Effects/ExplosionEffect.gd
@@ -1,10 +1,16 @@
 extends Node2D
 
+onready var particle1 = $Particles2D
+onready var particle2 = $Particles2D/Particles2D
+onready var particle3 = $Particles2D/Particles2D2
 
 onready var decayTimer = $Timer
 
 func _ready():
 	decayTimer.start()
+	particle1.set_emitting(true)
+	particle2.set_emitting(true)
+	particle3.set_emitting(true)
 
 
 
