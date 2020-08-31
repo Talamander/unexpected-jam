2ecd89648c1b67e6f288ab24bde5d7ec378e5062
Talamander <toxcrewproductions@gmail.com>
2020-08-31 17:44:28 -0500

    Basic Enemy Respawn Timer
    This definitely has some issues and needs to be overhauled but it works for now

--- Level/Arena.gd
+++ Level/Arena.gd
@@ -1,5 +1,16 @@
 extends Node2D
 
+var basicEnemy = preload("res://Enemies/BasicEnemy.tscn")
+
 #Stuff later
 func _ready():
 	Music.list_play()
+
+
+func _on_EnemySpawnTimer_timeout():
+	var enemy_position = Vector2(rand_range(-160, 670), rand_range(-90, 390))
+	
+	while enemy_position.x <640 and enemy_position.x > -80 or enemy_position.y < 360 and enemy_position.y > -45:
+		enemy_position = Vector2(rand_range(-160, 670), rand_range(-90, 390))
+	
+	Global.instance_scene_on_main(basicEnemy, enemy_position)
