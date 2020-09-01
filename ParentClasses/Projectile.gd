482ebf9ceac95a82533849f283df317f2843c307
Talamander <toxcrewproductions@gmail.com>
2020-08-31 19:20:31 -0500

    Misc Changes

--- ParentClasses/Projectile.gd
+++ ParentClasses/Projectile.gd
@@ -5,9 +5,6 @@ var velocity = Vector2.ZERO
 func _process(delta):
 	position += velocity * delta
 
-#Destroys the projectile after it has exited the viewport
-func _on_VisibilityNotifier2D_viewport_exited(_viewport):
-	queue_free()
 
 
 func _on_Hitbox_body_entered(_body):
