bf75f68ed24cb5e8e566fe6b3047b48e0b65852a
Talamander <toxcrewproductions@gmail.com>
2020-09-09 15:08:32 -0500

    ItemPickup & Muzzle particles now queue_free

--- Effects/MuzzleFlash.gd
+++ Effects/MuzzleFlash.gd
@@ -1,6 +1,12 @@
 extends Node2D
 
 onready var particles = $Particles2D
+onready var decayTimer = $Timer
 
 func _ready():
 	particles.emitting = true
+	decayTimer.start()
+
+
+func _on_Timer_timeout():
+	queue_free()
