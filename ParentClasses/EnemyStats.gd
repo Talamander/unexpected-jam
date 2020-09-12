cba60af96ca7848ed0f1e0a1690092c2bd150432
Doc-designs <70421267+Doc-designs@users.noreply.github.com>
2020-09-12 09:28:45 -0700

    Enemy Health Check

--- ParentClasses/EnemyStats.gd
+++ ParentClasses/EnemyStats.gd
@@ -7,6 +7,6 @@ onready var health = max_health setget set_health
 
 func set_health(value):
 	health = clamp(value, 0, max_health)
-	if health == 0:
+	if health <= 0:
 		emit_signal("enemy_died")
 		Global.waveRunner()
