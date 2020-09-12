20f621989bbcb516b8f7163e382f2ae489747d62
Talamander <toxcrewproductions@gmail.com>
2020-09-12 14:56:02 -0500

    Global.itemDrop func tweak

--- Singletons/Global.gd
+++ Singletons/Global.gd
@@ -63,7 +63,7 @@ func itemDrop(position):
 	itemDropRates = randi()%20+1
 	match itemDropRates:
 		1,2,3:
-			Global.instance_scene_on_main(healthItem, position)
+			instance_scene_on_main(healthItem, position)
 			
 		4,5,6,7:
-			Global.instance_scene_on_main(ammoItem, position)
+			instance_scene_on_main(ammoItem, position)
