1674197430dbb972bc5341f946c73a595e99f720
Doc-designs <70421267+Doc-designs@users.noreply.github.com>
2020-09-12 17:21:15 -0700

    New Hint and readded Mod List

--- Level/Arena.gd
+++ Level/Arena.gd
@@ -57,7 +57,9 @@ var instancetimer = null
 var instancetimerlength = 15
 var distanceFromPlayer = 0
 var maxDistanceFromPlayer = 950
-var modifierList = ["chargeShot"]
+var modifierList = ["SlowMotion", "PlayerDamageIncrease", "ReverseMovement", "RecoilRange",
+"NoRecoil", "BasicRuleSet", "killSwitch", "noMiniMap", "infiniteAmmo", "twoShot",
+"chargeShot"]
 
 #Stuff later
 func _ready():
